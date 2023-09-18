43ef5d6dbf2dfb7606406dd9e8a7a8296b44a388
heispapijay <heispapijay@gmail.com>
2023-09-18 23:31:32 +0300

    brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './brand.css';
+import { google, slack, atlassian, dropbox, shopify } from './import';
 
 function Brand() {
   return (
-    <div>
-      Brand
+    <article className="woonders_container-center with_padding">
+      <h2 className="woonders_text-color1">We are blessed to work with<br /> leading brands.</h2>
+    <div className="woonders_brand section_padding">
+      <div>
+        <img src={google} alt='google_logo' />
+      </div>
+      <div>
+        <img src={slack} alt='slack_logo' />
+      </div>
+      <div>
+        <img src={atlassian} alt='atlassian_logo' />
+      </div>
+      <div>
+        <img src={dropbox} alt='dropbox_logo' />
+      </div>
+      <div>
+        <img src={shopify} alt='shopify_logo' />
+      </div>
     </div>
+    </article>
   )
 }
 
